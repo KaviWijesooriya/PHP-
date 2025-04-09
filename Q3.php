5f60d05186d85b970f96a329ff9c75965b679900
KaviWijesooriya <162117406+KaviWijesooriya@users.noreply.github.com>
2025-04-09 15:17:36 +0530

    Add files via upload

---
+++ Q3.php
@@ -0,0 +1,30 @@
+
+<?php
+
+    $var01 = "DTK Academy";
+    
+?>
+<!DOCTYPE>
+<html>
+<head>
+        <title>
+            <?php
+
+echo $var01 ;
+?>
+</title>
+</head>
+<body>
+
+    
+ <h1>   <?php
+   
+echo $var01 ;
+    ?> </h1>
+    
+    <a href ="#">   <?php
+   
+   echo $var01 ;
+       ?> </a>
+</body>
+</html>
